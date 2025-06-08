315ed019259e8a4b545d99c029a2824fa210bcf3
rajalakshmys-27 <rajalakshmys27@gmail.com>
2025-06-08 19:36:51 +0530

    feat: reorganize icon components into categories for improved readability and maintainability

--- src/icons/index.jsx
+++ src/icons/index.jsx
@@ -1,3 +1,4 @@
+// Theme Icons
 export function SunIcon() {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#FFFFFF" className="size-6">
@@ -14,6 +15,15 @@ export function MoonIcon() {
     )
 }
 
+// Navigation/UI Control Icons
+export function MenuIcon({ className = '', ...props }) {
+    return (
+        <svg className={className} fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24" {...props}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+        </svg>
+    );
+}
+
 export function ChevronDownIcon({ className = '', ...props }) {
     return (
         <svg
@@ -25,12 +35,12 @@ export function ChevronDownIcon({ className = '', ...props }) {
     );
 }
 
+// Action Icons
 export function BookmarkIcon({ className = '', ...props }) {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`size-5 stroke-black dark:stroke-white ${className}`} {...props}>
             <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
         </svg>
-
     );
 }
 
@@ -38,14 +48,6 @@ export function BookmarkFilledIcon({ className = '', ...props }) {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={`size-6 dark:stroke-white ${className}`} style={{ fill: 'black' }} {...props}>
             <path fillRule="evenodd" d="M6 3a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3V6a3 3 0 0 0-3-3H6Zm1.5 1.5a.75.75 0 0 0-.75.75V16.5a.75.75 0 0 0 1.085.67L12 15.089l4.165 2.083a.75.75 0 0 0 1.085-.671V5.25a.75.75 0 0 0-.75-.75h-9Z" clipRule="evenodd" />
-        </svg>
-    );
-}
-
-export function CloseIcon({ className = '', ...props }) {
-    return (
-        <svg className={`w-5 h-5 text-red-500 ${className}`} fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24" {...props}>
-            <path d="M6 18L18 6M6 6l12 12" />
         </svg>
     );
 }
@@ -58,6 +60,14 @@ export function SearchIcon({ className = '', ...props }) {
     );
 }
 
+export function CloseIcon({ className = '', ...props }) {
+    return (
+        <svg className={`w-5 h-5 text-red-500 ${className}`} fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24" {...props}>
+            <path d="M6 18L18 6M6 6l12 12" />
+        </svg>
+    );
+}
+
 export function TrashIcon({ className = '', ...props }) {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`size-6 ${className}`} {...props}>
@@ -66,15 +76,34 @@ export function TrashIcon({ className = '', ...props }) {
     );
 }
 
+// Document/Content Icons
 export function DocumentMagnify({ className = '', ...props }) {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`size-7 dark:stroke-white ${className}`} {...props}>
             <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m5.231 13.481L15 17.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v16.5c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Zm3.75 11.625a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
         </svg>
-
     );
 }
 
+// User Interface Icons
+export function EyeIcon({ className = '', ...props }) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`w-5 h-5 text-gray-500 ${className}`} {...props}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+        </svg>
+    );
+}
+
+export function EyeOffIcon({ className = '', ...props }) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`w-5 h-5 text-gray-500 ${className}`} {...props}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88" />
+        </svg>
+    );
+}
+
+// User/Profile Icons
 export function ProfileIcon() {
     return (
         <svg className="w-6 h-6 text-blue-700 dark:text-blue-200" fill="currentColor" viewBox="0 0 24 24">
@@ -92,23 +121,7 @@ export function VerificationIcon() {
     )
 }
 
-export function EyeOffIcon({ className = '', ...props }) {
-    return (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`w-5 h-5 text-gray-500 ${className}`} {...props}>
-            <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88" />
-        </svg>
-    );
-}
-
-export function EyeIcon({ className = '', ...props }) {
-    return (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`w-5 h-5 text-gray-500 ${className}`} {...props}>
-            <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
-            <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-        </svg>
-    );
-}
-
+// Communication Icons
 export function MailIcon({ className = '', ...props }) {
     return (
         <svg className={className} fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" {...props}>
@@ -121,14 +134,6 @@ export function WarningIcon({ className = '', ...props }) {
     return (
         <svg className={className} fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" {...props}>
             <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v2m0 4h.01M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25H4.5a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-.659 1.591l-7.5 7.5a2.25 2.25 0 01-3.182 0l-7.5-7.5A2.25 2.25 0 012.25 6.993V6.75" />
-        </svg>
-    );
-}
-
-export function MenuIcon({ className = '', ...props }) {
-    return (
-        <svg className={className} fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24" {...props}>
-            <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
         </svg>
     );
 }
